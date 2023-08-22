eedfa544d20d7003292afbe49aa503a49c3d744a
Roger Carrijo <roger@birde.com.br>
2023-08-22 15:54:34 -0300

    initial update

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import numpy as np
 from PalmsGui import PalmsGui
-from palms.async_serial_comu import start
-import asyncio
-import serial_asyncio
 
-#pyinstaller --specpath palms.spec -F main.py
+# pyinstaller --specpath palms.spec -F main.py
 
 
 def main():
-    
     palms = PalmsGui()
+    palms.run()
+
 
 if __name__ == "__main__":
     main()
